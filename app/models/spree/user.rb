fd50f326d715d491163148b9fd8c30281b819d67
LiJunjie <holysoros@gmail.com>
2016-03-31 16:00:09 +0800

    Add email and password validations in Spree::User model

--- app/models/spree/user.rb
+++ app/models/spree/user.rb
@@ -3,5 +3,11 @@ module Spree
     self.table_name = 'spree_people'
 
     authenticates_with_sorcery!
+
+    validates :password, length: { minimum: 6 }, if: -> { new_record? || changes[:crypted_password] }
+    validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
+    validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
+
+    validates :email, uniqueness: true
   end
 end
